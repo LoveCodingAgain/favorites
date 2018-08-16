81106f7b9540a490f9ba7473ebe9ed0830735be3
LiXing <2212640110@qq.com>
2018-08-16 15:19:57 +0800

    初始提交

---
+++ src/main/java/com/favorites/FavoritesWebApplication.java
@@ -0,0 +1,12 @@
+package com.favorites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoritesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoritesWebApplication.class, args);
+	}
+}
